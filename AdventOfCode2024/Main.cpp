b3d84753ab981a3fd60680db35085652a52ac97b
Todor <stodor89@gmail.com>
2024-12-01 10:04:24 +0200

    Initial commit

---
+++ AdventOfCode2024/Main.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int totalDistance(const vector<int>& sortedNums1, const vector<int>& sortedNums2) {
+    int totalDistance = 0;
+    for (int i = 0; i < sortedNums1.size(); i++) {
+        totalDistance += abs(sortedNums1[i] - sortedNums2[i]);
+    }
+
+    return totalDistance;
+}
+
+int similarityScore(vector<int>& sortedNums1, vector<int> sortedNums2) {
+    int i = 0, j = 0, similarityScore = 0;
+    while (i < sortedNums1.size() && j < sortedNums2.size()) {
+        const int value = sortedNums1[i];
+        while (j < sortedNums2.size() && sortedNums2[j] < value) {
+            j++;
+        }
+
+        int nums2Occurrences = 0;
+        while (j < sortedNums2.size() && sortedNums2[j] == value) {
+            nums2Occurrences++;
+            j++;
+        }
+
+        int nums1Occurrences = 0;
+        while (i < sortedNums1.size() && sortedNums1[i] == value) {
+            nums1Occurrences++;
+            i++;
+        }
+
+        similarityScore += value * nums1Occurrences * nums2Occurrences;
+    }
+
+    return similarityScore;
+}
+
+int main(int argc, char* argv[]) {
+    vector<int> locations1, locations2;
+
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <filename>" << endl;
+        return 0;
+    }
+
+    ifstream file(argv[1]);
+    if (!file) {
+        cout << "Could not open " << argv[1] << endl;
+        return 1;
+    }
+
+    int loc1, loc2;
+    while (file >> loc1 >> loc2) {
+        locations1.push_back(loc1);
+        locations2.push_back(loc2);
+    }
+
+    sort(locations1.begin(), locations1.end());
+    sort(locations2.begin(), locations2.end());
+
+    cout << "total distance: " << totalDistance(locations1, locations2) << endl
+        << "similarity score: " << similarityScore(locations1, locations2) << endl;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode2024
+ My solutions to Advent of Code 2024
